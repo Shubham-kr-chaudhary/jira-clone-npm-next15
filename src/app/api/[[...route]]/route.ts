5d7e67213f7d6b9dafbda4b25404be6fb5096651
Shubham Kumar Chaudhary <shubhamkumchaudhary@gmail.com>
2025-09-23 16:58:02 +0530

    fixed test3

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,29 +1,52 @@
-import {Hono} from "hono";
-import {handle} from "hono/vercel";
+// import {Hono} from "hono";
+// import {handle} from "hono/vercel";
+// import auth from "@/features/auth/server/route";
+// import workspaces from"@/features/workspaces/server/route";
+// import members from "@/features/members/server/route";
+// import projects from "@/features/projects/server/route";
+// import tasks from "@/features/tasks/server/route";
+
+// const app= new Hono().basePath("/api")
+
+
+
+// const routes = app
+//  .route("/auth",auth)
+//  .route("/workspaces",workspaces)
+//  .route("/members",members)
+//  .route("/projects",projects)
+//  .route("/tasks",tasks);
+
+
+ 
+// export const GET = handle(app);
+// export const POST = handle(app);
+// export const PATCH = handle(app);
+// export const DELETE = handle(app);
+
+
+// export type AppType = typeof routes;
+
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
 import auth from "@/features/auth/server/route";
-import workspaces from"@/features/workspaces/server/route";
+import workspaces from "@/features/workspaces/server/route";
 import members from "@/features/members/server/route";
 import projects from "@/features/projects/server/route";
 import tasks from "@/features/tasks/server/route";
 
-const app= new Hono().basePath("/api")
+const app = new Hono().basePath("/api");
 
+app
+  .route("/auth", auth)
+  .route("/workspaces", workspaces)
+  .route("/members", members)
+  .route("/projects", projects)
+  .route("/tasks", tasks);
 
-
-export const routes = app
- .route("/auth",auth)
- .route("/workspaces",workspaces)
- .route("/members",members)
- .route("/projects",projects)
- .route("/tasks",tasks);
-
-
- 
 export const GET = handle(app);
 export const POST = handle(app);
 export const PATCH = handle(app);
 export const DELETE = handle(app);
 
-
-export type AppType = typeof routes;
-
+export type AppType = typeof app;
